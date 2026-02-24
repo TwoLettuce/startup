106e1ac80799d8d29764251be728092a73654ee6
TwoLettuce <grantharris0323@gmail.com>
2026-02-24 13:07:07 -0700

    player is now notified if they lack the mana for certain skills

--- src/play/WebSocketText.jsx
+++ src/play/WebSocketText.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
+//{props.messages}
 export function WebSocketText(props){
+    function parseMessages(messages){
+        let text = '';
+        for (const message of messages){
+            text = text + message + "\n";
+        }
+
+        return text;
+    }
     return (
-        <textarea readOnly rows="3" cols={40}>
-            {props.messages}
-        </textarea>
+        <textarea name="websocket" readOnly rows="3" cols={40} value={parseMessages(props.messages)} />
     )
 }
