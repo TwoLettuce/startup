f077ed645a8113136dc7958949a0de76949f0676
TwoLettuce <grantharris0323@gmail.com>
2026-02-23 23:07:14 -0700

    winning and losing a game now causes you to change state

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import "./play.css"
 import { CharacterSelect } from './CharacterSelect';
-import { Gameplay } from './Gameplay'
+import { Gameplay } from './Gameplay';
+import { Defeat } from './Defeat';
+import { Victory } from './Victory';
 
 export function Play(props) {
     const [character, setCharacter] = React.useState('none');
     const [enemyUsername, setEnemyUsername] = React.useState('Dr. Jensen');
     const [enemyCharacter, setEnemyCharacter] = React.useState('none');
     const [confirmed, setConfirmed] = React.useState(false);
+    const [gameWon, setGameWon] = React.useState(false);
+    const [gameLost, setGameLost] = React.useState(false);
 
     function resignConfirmation(){
         return (
@@ -23,14 +27,24 @@ export function Play(props) {
                 setEnemyCharacter={setEnemyCharacter} 
                 setConfirmed={setConfirmed} />
             }
-            {confirmed && 
-                <Gameplay username={props.username} character={character} enemyCharacter={enemyCharacter} enemyUsername={enemyUsername}/>
+            {confirmed && !gameLost && !gameWon &&
+                <div>
+                    <Gameplay username={props.username} 
+                    character={character} 
+                    enemyCharacter={enemyCharacter} 
+                    enemyUsername={enemyUsername}
+                    setGameWon={setGameWon}
+                    setGameLost={setGameLost} />
+                    <div id="resign-button">
+                        <form onClick={resignConfirmation()} method="delete" action="menu">
+                            <button id="resign">Resign game</button>
+                        </form>
+                    </div>
+                </div>
             }
-            <div id="resign-button">
-                <form onClick={resignConfirmation()} method="delete" action="menu">
-                    <button id="resign">Resign game</button>
-                </form>
-            </div>
+            {gameWon && <Victory />}
+            {gameLost && <Defeat />}
+            
         </main>
     );
 }
