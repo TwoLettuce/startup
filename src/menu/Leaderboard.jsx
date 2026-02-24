20650cf319d679d2b2322bb410b674014f44b31d
TwoLettuce <grantharris0323@gmail.com>
2026-02-24 13:48:05 -0700

    adding new files to the commit

---
+++ src/menu/Leaderboard.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export function Leaderboard(){
+    return (
+        <section id="leaderboard">
+            <label>Leaderboard</label>
+            <div>
+                <label>Search: </label>
+                <input type="search" placeholder="username"/>
+            </div>
+            <div>
+                <table>
+                <thead>
+                    <th>Ranking</th>
+                    <th>Name</th>
+                    <th>W/L</th>
+                </thead>
+                <thead>
+                    <td>#1</td>
+                    <td>nerdslayer</td>
+                    <td>68W/1L</td>
+                </thead>
+                <thead>
+                    <td>#2</td>
+                    <td>tryhard</td>
+                    <td>60W/42L</td>
+                </thead>
+                <thead>
+                    <td>#3</td>
+                    <td>theLegend27</td>
+                    <td>27W/27L</td>
+                </thead>
+                <thead>
+                    <td>#4</td>
+                    <td>trevor</td>
+                    <td>2W/75L</td>
+                </thead>
+                </table>
+            </div>
+            </section>
+    )
+    
+}
